8cddbaad10905f1ad46588660673bad5a0c87d0d
silaskolo <Ps123456>
2017-10-15 13:24:34 +0100

    Created and Included Application css and Javascript files

--- includes/header.php
+++ includes/header.php
@@ -2,5 +2,6 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/style.css" type="text/css">
 </head>
 <body>
